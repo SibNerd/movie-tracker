b08348f4a9d0f6811c3902352fb152afa8894c5b
SibNerd <yaremiarem@gmail.com>
2024-11-01 23:27:36 +0300

    update config file

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,9 @@
 package config
 
 type Config struct {
-	SearcherURL string `env:"SEARCHER_URL" envDefault:"https://api.tvmaze.com/search/shows"`
+	SearcherURL      string `env:"SEARCHER_URL" envDefault:"https://api.tvmaze.com/search/shows"`
+	ImdbURL          string `env:"IMDB_URL" envDefault:"https://www.imdb.com/title/"`
+	DatabaseURL      string `env,required:"DATABASE_URL"`
+	DatabaseUser     string `env,required:"DATABASE_USER"`
+	DatabasePassword string `env,required:"DATABASE_PASSWORD"`
 }
